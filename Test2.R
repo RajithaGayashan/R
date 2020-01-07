160cc0482819da0698efb7c4d3360674cde77d69
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-07 22:03:03 +0530

    Basic functions

---
+++ Test2.R
@@ -0,0 +1,15 @@
+
+log(10) #base=exp(1)
+exp(1)  #e value
+exp(2)  #e^2 value
+log(exp(1))
+log10(100)
+log(100,base = 10)
+log(base = 10,100)
+
+sqrt(16) #square root
+sqrt(8)
+
+pi #pi value
+sin(pi/2)
+cos(pi/3)
