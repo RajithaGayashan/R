6bc39a2e8c688a0ad1229b56ee35682b924dc3b4
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-09 23:55:17 +0530

    Variable

---
+++ Test4.R
@@ -0,0 +1,14 @@
+#Variable
+first_name="Rajitha"
+firstName="Rajitha"
+firstname="Rajitha"
+2name="Rajitha" #error
+@name="Rajitha" #error
+
+y=2
+y
+print(y)
+y<-10
+y
+name<-"Gayashan"
+print(name)
