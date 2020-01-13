747ad8d4e22bf4dbb79b7794079e440b3645f1ec
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-14 00:32:35 +0530

    iris dataframe

---
+++ Test8.R
@@ -0,0 +1,12 @@
+#iris dataframe
+iris
+head(iris)
+tail(iris)
+iris$Sepal.Length
+str(iris)
+summary(iris)
+summary(iris$Petal.Length)
+summary(iris$Species)
+class(iris)
+class(iris$Species)
+class(iris$Petal.Length)
