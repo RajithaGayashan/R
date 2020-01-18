a7c74b9ae1175a7cca754575e9fac1649f74c577
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-18 21:38:49 +0530

    Data summarization -Graphical method

---
+++ Test12.R
@@ -0,0 +1,5 @@
+#Data summarization -Graphical method
+#pie charts
+program<-c("Accounting","Bussiness","Programming","Teaching")
+student<-c(422,400,653,109)
+pie(student,main="Student count",lebels=program)
