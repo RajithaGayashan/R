389d7fbaf0ccead12f751fa3f19269e69db715c7
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-25 04:25:24 +0530

    Data summarization -Graphical method-Histogram

---
+++ Test15.R
@@ -0,0 +1,6 @@
+#Data summarization -Graphical method
+#Histogram
+weight<-c(123,123,175,152,200,198,178,147,197,167,146,137,138,178,196,148,195,187,182)
+hist(weight)
+iris
+hist(iris$Sepal.Length)
